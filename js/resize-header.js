3405663622e0c51dd1ba49fda5a7311a2b242635
Kennith Nichol <ken@kennithnichol.com>
2014-04-10 22:37:46 -0700

    Update resize-header.js
    Included conditional check on the image source which ensures the script does not trigger a resource all to "/none".

--- js/resize-header.js
+++ js/resize-header.js
@@ -1,4 +1,3 @@
-
 /*
  *  This scripts prevents sidebars from being cutoff if the content is not longer than the sidebars
  */
@@ -6,58 +5,39 @@
 /*
  * finds the height of the #sidebar and #sidebar-alt the sets the #content to have a min-height of whichever value is greater
  */
-function grhHeaderAutoHeight(){
+function grhHeaderAutoHeight()
+{
+    var headerImage = new Image(),
+        imageSource = jQuery('#title-area').css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
 
-	
-	 /*   var imageSrc = document
-                    .getElementById('#title-area')
-                     .style
-                      .backgroundImage
-                       .replace(/url\((['"])(.*?)\1\)/gi, '$2')
-                        .split(',')[0];
+    // Make sure we're not trying to load any empty image source
+    // This will prevent calls to /none 
+    if (typeOf(imageSource) !== 'undefined' && imageSource !== '' && imageSource !== 'none') {
+        headerImage.src = imageSource;
+        if(headerImage) {
+            var width = headerImage.width,
+                currentWidth = jQuery( '#title-area' ).width(),
+                ratio = currentWidth/width,
+                height = headerImage.height*ratio,
+                body = jQuery( 'body' ).width();
 
-    // I just broke it up on newlines for readability        
-
-    var image = new Image();
-    image.src = imageSrc;*/
-    
-    var headerImage = new Image();
-    headerImage.src = jQuery('#title-area').css('background-image').replace(/"/g,"").replace(/url\(|\)$/ig, "");
-    
-    
-    
-    if(headerImage) {
-
-	    var width = headerImage.width,
-	        currentWidth = jQuery( '#title-area' ).width(),
-	        ratio = currentWidth/width,
-	        height = headerImage.height*ratio,
-	        body = jQuery( 'body' ).width();
-        
-    
-	        //alert(height);
-	
-		if( body < 768 ){
-	    
-			if( ratio < 1 ){
-				jQuery( '#title-area' ).attr( 'style', 'height: '.concat( height, 'px !important' ) );
-			}
-	
-		}
-	
-	}
-	
-	
+            if( body < 768 ){
+                if( ratio < 1 ){
+                    jQuery( '#title-area' ).attr( 'style', 'height: '.concat( height, 'px !important' ) );
+                }
+            }
+        }
+    }
 }
 
-jQuery(document).ready(function($){
-
+jQuery(document).ready(function($)
+{
     $( 'body' ).attr( 'onload', 'grhHeaderAutoHeight()' );
-
 });
 
 var resizeTimer;
-jQuery(window).resize(function() {
+jQuery(window).resize(function()
+{
 	clearTimeout(resizeTimer);
 	resizeTimer = setTimeout(grhHeaderAutoHeight, 100);
 });
